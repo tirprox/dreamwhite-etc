7694e24918c73440d9c5cfe33239ee40dad7472d
tirprox <samsonov.gleb@gmail.com>
2017-10-23 16:54:56 +0300

    plugin changes

--- import-export/woocommerce-import-export.php
+++ import-export/woocommerce-import-export.php
@@ -28,12 +28,12 @@ function addImportMenu() {
    ?>
    <div class="wrap">
       <h1>Импорт</h1>
-      <form action="/wp-content/plugins/import-export/import-products/import.php" method="post">
+      <form action="/wp-content/plugins/import-export/import-products/makeReport.php" method="post">
          <?php settings_fields('plugin_options'); ?>
          <?php do_settings_sections('plugin'); ?>
          <input name="run-import" type="submit" value="Импортировать!" />
       </form></div>
-   <?php
+	<?php
 }
 
 function runImport() {
