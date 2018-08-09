5cd95d0669e16ede231a00e2e072e9ec91238afd
tirprox <samsonov.gleb@gmail.com>
2018-08-09 16:40:50 +0300

    Filter with query manager, Tax params

--- plugins/product-filter/product-filter.php
+++ plugins/product-filter/product-filter.php
@@ -12,134 +12,198 @@ Text Domain: dw-product-filter
 Domain Path: /languages
 */
 
-//namespace Dreamwhite\Site;
+init();
 
-function custom_seo_taxonomy()  {
+function init()
+{
+    add_action('init', 'dw_custom_seo_taxonomy', 0);
+    add_shortcode('custom_filter', 'dw_filter_tags_shortcode');
+}
+
+function dw_custom_seo_taxonomy()
+{
 
     $labels = array(
-        'name'                       => 'SEO-Метки',
-        'singular_name'              => 'SEO-Метка',
-        'menu_name'                  => 'SEO-Метки',
-        'all_items'                  => 'Все SEO-Метки',
-        'parent_item'                => 'Родительская',
-        'parent_item_colon'          => 'Родительская:',
-        'new_item_name'              => 'Новая метка',
-        'add_new_item'               => 'Добавить метку',
-        'edit_item'                  => 'Редактировать',
-        'update_item'                => 'Обновить',
+        'name' => 'SEO-Метки',
+        'singular_name' => 'SEO-Метка',
+        'menu_name' => 'SEO-Метки',
+        'all_items' => 'Все SEO-Метки',
+        'parent_item' => 'Родительская',
+        'parent_item_colon' => 'Родительская:',
+        'new_item_name' => 'Новая метка',
+        'add_new_item' => 'Добавить метку',
+        'edit_item' => 'Редактировать',
+        'update_item' => 'Обновить',
         'separate_items_with_commas' => 'Separate Brand with commas',
-        'search_items'               => 'Поиск по меткам',
-        'add_or_remove_items'        => 'Add or remove Brands',
-        'choose_from_most_used'      => 'Choose from the most used Brands',
+        'search_items' => 'Поиск по меткам',
+        'add_or_remove_items' => 'Add or remove Brands',
+        'choose_from_most_used' => 'Choose from the most used Brands',
     );
     $args = array(
-        'labels'                     => $labels,
-        'hierarchical'               => true,
-        'public'                     => true,
-        'show_ui'                    => true,
-        'show_admin_column'          => true,
-        'show_in_nav_menus'          => true,
-        'show_tagcloud'              => true,
+        'labels' => $labels,
+        'hierarchical' => true,
+        'public' => true,
+        'show_ui' => true,
+        'show_admin_column' => true,
+        'show_in_nav_menus' => true,
+        'show_tagcloud' => true,
+        'rewrite' => [
+            'slug' => 'catalog',
+            'with_front' => false
+        ]
     );
-    register_taxonomy( 'attr', 'product', $args );
+    register_taxonomy('attr', 'product', $args);
 
 }
 
-add_action( 'init', 'custom_seo_taxonomy', 0 );
+function dw_filter_tags_shortcode()
+{
 
-// Add Shortcode
-function custom_shortcode() {
+    echo '<div style="padding: 8px 16px">';
 
     $tax = get_queried_object();
-    $meta = get_term_meta($tax->term_id);
+
+    $params = new TaxonomyParams($tax);
+
+    $queryManager = new QueryManager();
+    $queryManager->fromTaxonomyParams($params);
 
     $term_id = $tax->term_id;
 
-    echo '<h4>TERM ID</h4>';
-    echo $tax->term_id . '<br>';
+
+    $args = $queryManager->getQueryArgs();
 
 
-    echo '<h4>Цвет</h4>';
-    echo '<div class="variation-colors">';
 
-    $colors = explode(',', $meta['color'][0]);
-
-    $params = [
-      'color' => 'Черный',
-    ];
-
-    set_query_var('color', 'Черный');
-    set_query_var('uteplitel', 'Нет');
-
-    //set_query_var('type', 'Пальто');
-    //set_query_var('gender', 'Женский');
-
-    foreach ($colors as $color) {
-        echo $color . '<br>';
-    }
-    echo '<br>';
-
-    $q_color = get_query_var( 'color');
-    $q_uteplitel = get_query_var( 'uteplitel');
-
-
-    $args = array(
-        'taxonomy'   => 'attr',
-        'hide_empty' => false,
-        'meta_query' => array(
-            array(
-                'key'       => 'color',
-                'value'     => $q_color,
-                'compare'   => 'LIKE'
-            ),
-            [
-                'key'       => 'uteplitel',
-                'value'     => $q_uteplitel,
-                'compare'   => 'LIKE'
-            ],
-        )
-    );
+    Renderer::header('Matching Taxonomies:');
 
     $terms = get_terms($args);
+    foreach ($terms as $term) {
+        Renderer::a($term->name, get_term_link($term));
+    }
+
+
+    /*if (isset($terms) && $terms[0]->term_id !== $term_id) {
+        wp_redirect(get_term_link($terms[0]->term_id));
+    }*/
 
     echo '</div>';
-    echo '<h4>QUERY VAR</h4>';
-    echo $q_color . '<br>';
-
-    echo '<h4>TAXES</h4>';
-
-    foreach ($terms as $term) {
-        echo $term->name . '<br>';
-    }
-
-    echo '<br>';
-
-    if (isset($terms) && $terms[0]->term_id !== $term_id) {
-        wp_redirect(get_term_link($terms[0]->term_id));
-    }
-
-
 }
 
-add_shortcode( 'custom_filter', 'custom_shortcode' );
 
-function handle_custom_filter_var($query, $query_vars)
+class QueryManager
 {
-    if (!empty($query_vars['color'])) {
-        $query['meta_query'][] = array(
-            'key' => 'color',
-            'value' => esc_attr($query_vars['article']),
-        );
+
+    private $queryParams = [];
+
+    public function setQueryParameter($name, $value)
+    {
+        if($value !== '') {
+            $this->queryParams[$name] = $value;
+            set_query_var($name, $value);
+        }
+
     }
 
-    return $query;
+    public function getQueryParameter($name)
+    {
+        return get_query_var($name);
+    }
+
+    public function deleteQueryParameter($name)
+    {
+        unset($this->queryParams[$name]);
+        set_query_var($name, '');
+    }
+
+
+    // Accepts TaxonomyParams as an argument
+    public function fromTaxonomyParams($taxonomy)
+    {
+        foreach ($taxonomy->getParams() as $name => $value) {
+            $this->setQueryParameter($name, implode(',', $value));
+        }
+    }
+
+    public function getQueryArgs()
+    {
+        $metaQuery = [];
+
+        foreach ($this->queryParams as $name => $value) {
+            $metaQuery[] = [
+                'key' => $name,
+                'value' => $value,
+                'compare' => '='
+            ];
+        }
+
+        $args = [
+            'taxonomy' => 'attr',
+            'hide_empty' => false,
+            'meta_query' => $metaQuery
+        ];
+
+
+        return $args;
+    }
 }
 
-add_filter('woocommerce_product_data_store_cpt_get_products_query', 'handle_custom_filter_var', 10, 2);
+class TagRouter
+{
 
-
-function add_query_vars_filter( $vars ) {
-    $vars[] = "color";
-    return $vars;
 }
-add_filter( 'query_vars', 'add_query_vars_filter' );
+
+class TaxonomyParams
+{
+    private $params = [];
+
+    public function getParams()
+    {
+        return $this->params;
+    }
+
+    public function __construct($taxonomy)
+    {
+        $meta = get_term_meta($taxonomy->term_id);
+
+        foreach ($meta as $param => $value) {
+            $this->params[$param] = $value[0] !== '' ? explode(',', $value[0]) : [];
+//            echo $param . ": " . implode(',', $this->params[$param]) . '<br>';
+        }
+
+        foreach ($this->params as $param => $value) {
+            if (!empty($value)) {
+                Renderer::header($param);
+                foreach ($value as $item) {
+                    Renderer::tag($item);
+                }
+            }
+        }
+    }
+
+}
+
+class Renderer
+{
+
+    public static function tag($content)
+    {
+        echo "<div style='font-size: 12px; display: inline-block; padding: 0px 4px; margin: 2px;border: 1px solid #cccccc'>$content</div>";
+    }
+
+    public static function header($content)
+    {
+        echo "<h3 style=''>$content</h3>";
+    }
+
+    public static function p($content)
+    {
+        echo "<p style='font-size: 12px; margin-bottom: 4px'>$content</p>";
+    }
+
+    public static function a($content, $href)
+    {
+        echo "<a style='font-size: 12px; margin-bottom: 0px; display: block;' href='$href'>$content</a>";
+    }
+
+}
