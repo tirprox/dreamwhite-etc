e2500f5dd74d56ce0e562dc81ef01a591b089f03
glebsamsonov <samsonov.gleb@gmailcom>
2021-02-05 19:03:55 +0300

    renaming script -- added delete of attachments from wp

--- photo/rename.php
+++ photo/rename.php
@@ -1,143 +1,305 @@
 <?php
 
-$categoryDirNames = getDirectories(__DIR__);
+const FILE_FOLDER =  "dirs";
+const DELETE_ATTACHMENTS_URL = 'https://dreamwhite.ru/attachments/update-attachments.php';
 
-foreach ($categoryDirNames as $categoryDirName) {
-    $catDirPath = __DIR__ . "/" . $categoryDirName;
-    $articleDirNames = getDirectories($catDirPath, $catDirPath);
 
-    foreach ($articleDirNames as $articleDirName) {
+const BASE_PATH = __DIR__ . '/' .  FILE_FOLDER;
 
-        $articleDirPath = $catDirPath . "/" . $articleDirName;
-        $colorDirNames = getDirectories($articleDirPath, $articleDirPath);
+const DIR_PERM_CMD = "find " . FILE_FOLDER . " -type d -exec chmod 755 {} \;";
+const FILE_PERM_CMD = "find " . FILE_FOLDER . " -type f  -exec chmod 644 {} \;";
+$renamed_files = [];
 
-        if (!empty($colorDirNames)) {
-            foreach ($colorDirNames as $colorDirName) {
-                $colorDirPath = $articleDirPath . "/" . $colorDirName;
-                renameFiles(getFiles($colorDirPath), $articleDirName, $colorDirName, $colorDirPath);
-            }
-        }
+fix_permissions();
+set_extensions_to_lowercase();
+
+recursive_dirname_cleanup(BASE_PATH);
+report_empty_dirs(BASE_PATH);
+
+fix_filerun_path();
+request_delete_attachments($renamed_files);
+
+
+
+function fix_permissions() {
+
+  echo shell_exec( DIR_PERM_CMD);
+  echo shell_exec( FILE_PERM_CMD);
+}
+
+function set_extensions_to_lowercase() {
+  $sub = 'a=$(echo "$0" | sed -r "s/([^.]*)\$/\L\1/"); [ "$a" != "$0" ] && mv "$0" "$a" ';
+  $cmd = "find . -name '*.*' -exec sh -c '$sub' {} \;";
+  echo shell_exec($cmd);
+}
+
+function recursive_dirname_cleanup($path) {
+  global $renamed_files;
+
+  $dirs = get_dirs($path);
+  foreach ($dirs as $dir) {
+
+    $fixed_path = fix_dirname($dir);
+    recursive_dirname_cleanup($fixed_path);
+    rename_files_in_path($fixed_path, $renamed_files);
+
+  }
+}
+
+function get_dirs($path) {
+  return glob($path . '/*' , GLOB_ONLYDIR);
+}
+
+function fix_dirname($path) {
+  fix_multispace($path);
+  trim_dir($path);
+
+  return $path;
+}
+
+function fix_multispace(&$path) {
+  $dirname = basename($path); // Get dir name from path
+  $parent = dirname($path, 1);
+
+  $fixed = preg_replace('/\s+/', ' ', $dirname);
+
+  if ($fixed !== $dirname) {
+
+    $fixed_path = "$parent/$fixed";
+
+    rename($path, $fixed_path);
+
+    show_message("Переименован: '$dirname' -> '$fixed'", "(несколько пробелов подряд)");
+
+    $path = $fixed_path;
+  }
+
+}
+
+function trim_dir(&$path) {
+  $dirname = basename($path); // Get dir name from path
+  $parent = dirname($path, 1);
+
+  $trimmed = trim($dirname);
+
+  if ($trimmed !== $dirname) {
+
+    $trimmed_path = "$parent/$trimmed";
+
+    rename($path, $trimmed_path);
+
+    show_message("Переименован: '$dirname' -> '$trimmed'", "(лишний пробел)\n");
+    $path = $trimmed_path;
+  }
+}
+
+function show_message($message, $comment = "\n") {
+  if (php_sapi_name() === 'cli') {
+    echo "$message $comment";
+
+    if ($comment !== "\n") {
+      echo "\n";
     }
+  } else {
+    echo "<p>$message <b>$comment</b></p>";
+  }
 }
 
-function getDirectories($directory, $prefix = "") {
-    $files = array_diff(scandir($directory), array('..', '.'));
+function report_empty_dirs($path) {
+  $output = explode("\n", trim(shell_exec( "cd $path && find . -type d -empty")));
+  show_message("-------------");
+  show_message("Пустые папки:");
+  foreach ($output as $line) {
+    show_message($line);
+  }
+}
 
-    $dirs = [];
-    foreach ($files as $file) {
-        if ($prefix !== "") {
-            if (is_dir($prefix . "/" . $file)) {
-                $dirs[] = $file;
-            }
+function fix_filerun_path() {
+  shell_exec("/opt/plesk/php/7.4/bin/php ./cron/paths_cleanup.php");
+}
+
+
+function request_delete_attachments($renamed_files) {
+
+  if (count($renamed_files) <= 0) {
+    return;
+  }
+
+  $json = json_encode($renamed_files, JSON_UNESCAPED_UNICODE);
+
+  $options = array(
+    'http' => array(
+      'header'  => "Content-type: application/json\r\n",
+      'method'  => 'POST',
+      'content' => $json
+    )
+  );
+
+  $context  = stream_context_create($options);
+
+  $result = file_get_contents(DELETE_ATTACHMENTS_URL, false, $context);
+
+  if ($result === false) {
+    show_message("Запрос на удаление фото с сайта не удался");
+  }
+
+  print_r($result);
+
+}
+
+
+function rename_files_in_path($path, &$renamed) {
+  $fdir = basename(dirname($path, 3));
+
+  if ($fdir === FILE_FOLDER) {
+    $color = basename($path); // Get dir name from path
+    $article = basename(dirname($path, 1));
+//    $category = basename(dirname($path, 2));
+
+    $prefix = "$article-$color-";
+
+    // File sorting buckets:
+    $preordered = []; // 1.jpg
+    $existing = []; // Черный-1.jpg
+    $random = []; // asd123.jpg
+
+
+    $file_count = 0;
+
+    $position =1;
+    foreach (new DirectoryIterator($path) as $file) {
+      if ($file->isFile()) {
+        if ($file->isDot()) continue;
+
+        $file_count++;
+
+        $strip_ext = str_replace('.' . $file->getExtension(), '', $file->getFilename());
+
+        $f = [];
+        $f['name'] = $file->getFilename();
+        $f['strip_ext'] = $strip_ext;
+        $f['ext'] = $file->getExtension() ?? '';
+        $f['inode'] = $file->getInode();
+
+
+
+
+        // 1. Sort files into buckets (auto priority)
+
+        if (ctype_digit($strip_ext)) {
+          // Preordered
+          $n = intval($strip_ext);
+
+//          echo "Preordered: $strip_ext, position: $n\n";
+          $f['preferred_position'] = $n;
+          $preordered[$f['name']] = $f;
+
+        }  else if (strpos($strip_ext, $prefix) !== false) {
+          // Existing
+          $n = intval(str_replace($prefix, '', $strip_ext));
+//          echo "Existing: $strip_ext, position: $n\n";
+          $f['preferred_position'] = $n;
+          $existing[$f['name']] = $f;
+
+
+        } else {
+          // Random
+//          echo "Random: $strip_ext\n";
+          $f['preferred_position'] = 0;
+          $random[$f['name']] = $f;
+
         }
-        else {
-            if (is_dir($file)) {
-                $dirs[] = $file;
-            }
-        }
+      }
+    }
+
+
+
+    // 2. Assign numbers
+    $sorted = [];
+
+    for ($i = 1; $i <= $file_count; $i++ ) {
+      $sorted[$i] = null;
+    }
+
+    // if preordered file number is bigger then total amount of files, move it to random
+    foreach ($preordered as $f) {
+      if ($f['preferred_position'] > $file_count) {
+        $f['preferred_position'] = 0;
+        $random[$f['name']] = $f;
+        unset($preordered[$f['name']]);
+      } else {
+        $sorted[$f['preferred_position']] = $f;
+      }
+    }
+
+
+
+
+    $conflicting = [];
+
+    ksort($existing);
+
+    foreach ($existing as $f) {
+      // resolve position conflict
+      $pp = $f['preferred_position'];
+
+      if (($pp <= $file_count) && ($sorted[$f['preferred_position']] !== null)) {
+        $conflicting[$f['preferred_position']] = $f;
+      } else {
+        $sorted[$f['preferred_position']] = $f;
+      }
 
     }
-    return $dirs;
-}
 
-function getFiles($directory) {
-    return array_diff(scandir($directory), array('..', '.'));
-}
+    $available = array_reverse(array_keys($sorted, null, true));
 
-function renameFiles($files, $article, $color, $colorDir) {
-    if (!empty($files)) {
-        $i = 1;
-        $isBkp = false;
 
-        /* if file 1.jpg is in a folder */
-        if (in_array('1.jpg', $files) || in_array('1.JPG', $files)) {
-            $name = $article . "-" . $color . "-" . "1.jpg";
-
-            if (in_array($name, $files)) {
-                rename($colorDir . "/" . $name, $colorDir . "/" . 'bkp.jpg');
-                $isBkp = true;
-            }
-
-            rename($colorDir . "/" . "1.jpg", $colorDir . "/" . $name);
-            rename($colorDir . "/" . "1.JPG", $colorDir . "/" . $name);
-
-            $files = array_diff(scandir($colorDir), array('..', '.'));
-
-            if ($isBkp) {
-                // first randomizing names except for 1 and bkp
-                $j = 2;
-                foreach ($files as $file) {
-                    $path = $colorDir . "/" . $file;
-
-                    $newName = $j . ".jpg";
-                    if ($file !== $name && $file !== 'bkp.jpg') {
-                        rename($path, $colorDir . "/" . $newName);
-                    }
-                    $j++;
-                }
-                $name2 = $article . "-" . $color . "-" . "2.jpg";
-                rename($colorDir . "/" . "bkp.jpg", $colorDir . "/" . $name2);
-
-                $i = 3;
-                $files = array_diff(scandir($colorDir), array('..', '.'));
-
-                foreach ($files as $file) {
-                    $path = $colorDir . "/" . $file;
-
-                    $newName = $article . "-" . $color . "-" . $i . ".jpg";
-                    if ($file !== $name && $file !== $name2) {
-                        rename($path, $colorDir . "/" . $newName);
-                        $i++;
-                    }
-                }
-
-            }
-            else {
-                // first randomizing names except for 1 and bkp
-                $j = 2;
-                foreach ($files as $file) {
-                    $path = $colorDir . "/" . $file;
-
-                    $newName = $j . ".jpg";
-                    if ($file !== $name) {
-                        rename($path, $colorDir . "/" . $newName);
-                    }
-                    $j++;
-                }
-
-                $i = 2;
-                $files = array_diff(scandir($colorDir), array('..', '.'));
-
-                foreach ($files as $file) {
-                    $path = $colorDir . "/" . $file;
-
-                    $newName = $article . "-" . $color . "-" . $i . ".jpg";
-                    if ($file !== $name) {
-                        rename($path, $colorDir . "/" . $newName);
-                        $i++;
-                    }
-                }
-            }
-
-        }
-
-        /* if no file named 1.jpg present in a folder */
-        else {
-
-            $files = array_values($files);
-            $fileCount = count($files);
-            for ($j = 0; $j < $fileCount; $j++) {
-                $path = $colorDir . "/" . $files[$j];
-                $newName = $article . "-" . $color . "-" . $i . ".jpg";
-
-                if (!in_array($newName, $files)) {
-                    rename($path, $colorDir . "/" . $newName);
-                    $files[$j] = $newName;
-                }
-
-                $i++;
-            }
-        }
+    for ($i = 1; $i <= $file_count; $i++ ) {
+      if (array_key_exists($i, $conflicting )) {
+        $position = array_pop($available);
+        $sorted[$position] = $conflicting[$i];
+      }
     }
-}
 
+
+    foreach ($random as $file) {
+      $position = array_pop($available);
+      $sorted[$position] = $file;
+    }
+
+
+    $renaming_list = [];
+    foreach ($sorted as $position => $file) {
+      $new_name = $file['ext'] !== '' ? "$prefix$position." . $file['ext'] : "$prefix$position";
+      $sorted[$position]['new_name'] = $new_name;
+
+      if ($new_name !== $file['name']) {
+        $renaming_list[$file['name']] = $new_name;
+        $renamed[] = "$prefix$position";
+      }
+    }
+
+    // 3. Set random filenames
+    $r=[];
+
+    $rc = 1;
+    $renamed_count = 0;
+    foreach ($renaming_list as $name => $new_name) {
+      rename("$path/$name", "$path/$rc");
+      $r[$rc] = $new_name;
+      $rc++;
+
+      $renamed_count++;
+    }
+
+    // 4. Rename files in specified order by addressing their inode number
+    foreach ($r as $name => $new_name) {
+      rename("$path/$name", "$path/$new_name");
+
+    }
+
+    if ($renamed_count > 0) {
+      show_message("Переименован:", "$article $color");
+    }
+  }
+}
